1e8853ce465c5dc660cf2573ae6d513e39892917
Martín Rodríguez <monse.developer@gmail.com>
2024-03-21 21:57:19 -0600

    Update 3er Parcial

--- 2do Parcial/AgendaPersonal/AgendaPersonal/obj/Debug/netcoreapp2.2/AgendaPersonal.AssemblyInfo.cs
+++ 2do Parcial/AgendaPersonal/AgendaPersonal/obj/Debug/netcoreapp2.2/AgendaPersonal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgendaPersonal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a66ecf24bc739737d6b11af0b660db321241dc8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgendaPersonal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgendaPersonal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
